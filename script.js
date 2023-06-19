3792fe4f555d6d240206ab67a16d3d9f32b55c95
MewKay <125735249+MewKay@users.noreply.github.com>
2023-06-19 13:46:40 +0300

    Add a function to display the books onto the page

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 let myLibrary = [];
+const libraryDisplay = document.querySelector(".library-display");
 
 function Book(title,author,pages,read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read ? "already read" : "not read yet"}`;
-  }
 }
+
+addBookToLibrary("The Hobbit","J.R.R. Tolkien","295","Not read")
+addBookToLibrary("The Last Wish","Andrzej Sapkowski","288","Read")
+displayLibrary();
 
 function addBookToLibrary (title,author,pages,read) {
   let bookToAdd = new Book(title,author,pages,read);
   myLibrary.push(bookToAdd);
 }
 
-const theHobbit = new Book("The Hobbit","J.R.R. Tolkien","295",true);
-
-console.log(theHobbit.info());
+function displayLibrary () {
+  myLibrary.forEach(book => {
+    let bookContents = Object.values(book);
+    for(let i=0; i<4; i++) {
+      let content = document.createElement("div");
+      content.innerText = bookContents[i];
+      libraryDisplay.appendChild(content);
+    }
+  });
+}
